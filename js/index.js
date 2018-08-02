1214b73681011763464b7d202859f76043e40734
Bermudo Earvin Jay (FGA.CEBU) <earvin.bermudo@fsoft.com.vn>
2018-08-02 13:55:20 +0800

    added image for project div

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ Vue.component('vheader', {
 })
 
 Vue.component('vcontent', {
-    template: '<p>Hello there, I am me! Sorry about the design of my website.<br/> I just made this out of boredom :D .  haha <br/><br/>By the way, I\'m gonna tell you a little bit something about myself. <br/>  As you can see my name is on the top,<br/> and I\'m a Software Development Engineer.<br/>My hobbies are playing guitar , playing games <br/> and living life :D.<br/><br/>If you want to know more about me,<br/> you must not know more about me  :D </p>',
+    template: '<p>Hello there, I am me! Sorry about the design <br/> of my website. I just made this out of boredom :D .  haha <br/>By the way, I\'m gonna tell you a little bit something about myself. <br/>  As you can see my name is on the top,<br/> and I\'m a Software Development Engineer.<br/>My hobbies are playing guitar , playing games and living life :D.<br/>If you want to know more about me,<br/> you must not know more about me  :D </p>',
 })
 
 var app = new Vue({
@@ -24,30 +24,36 @@ var app = new Vue({
         },
         styleHeader: {
             display: 'inline-block',
-            margin: '25',
+            margin: '20',
             backgroundColor: '#ffe066',
             padding: 5,
             borderStyle: 'solid'
         },
         styleContent: {
-            marginTop:'12%',
-            marginLeft:'21%',
+            marginTop: '14%',
+            marginLeft: '21%',
             position: 'absolute'
         },
         profileName: 'Earvin Jay G. Bermudo',
     },
     methods: {
         onClickAboutMe: function (event) {
-            alert('About Me')
+            this.$refs.divAboutMe.style.display = 'block';
+            this.$refs.divProjects.style.display = 'none';
         },
         onClickProjects: function (event) {
-            alert('Projects')
+            this.$refs.divAboutMe.style.display = 'none';
+            this.$refs.divProjects.style.display = 'block';
         },
         onClickSkillSet: function (event) {
-            alert('Skills')
+            this.$refs.divAboutMe.style.display = 'none';
+            this.$refs.divProjects.style.display = 'none';
         }
     },
     created: function () {
-        console.log('created')
+       console.log('created');
+    },
+    mounted: function () {
+        this.$refs.divProjects.style.display = 'none';
     }
 });
