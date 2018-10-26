3712588cd4b9e216e80065de05dacaca481069a1
Bermudo Earvin Jay (FGA.CEBU) <earvin.bermudo@fsoft.com.vn>
2018-10-26 10:53:07 +0800

    added content to my project and skillset

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ Vue.component('vheader', {
 })
 
 Vue.component('vcontent', {
-    template: '<p>Hello there, I am me! Sorry about the design <br/> of my website. I just made this out of boredom :D .<br/>By the way, I\'m gonna tell you a little bit something about myself. <br/>  As you can see my name is on the top,<br/> and I\'m a Software Development Engineer.<br/>My hobbies are playing guitar , playing games and living life :D.<br/>If you want to know more about me,<br/> you must not know more about me  :D </p>',
+    template: '<p>Hello there, I am me! Sorry about the design <br/> of my website. I just made this out of boredom :D .<br/>By the way, I\'m gonna tell you a little bit something about myself. <br/>  As you can see my name is on the top,<br/> and I\'m a Software Development Engineer.<br/>My hobbies are playing guitar and playing computer/console games.<br/>If you want to know more about me,<br/> you must not know more about me  :D </p>',
 })
 
 var app = new Vue({
@@ -38,6 +38,8 @@ var app = new Vue({
         project3Name: 'ECR-MSA',
         project2Name: 'neurobeat',
         project1Name: 'Avatar',
+        underDev:'UNDER DEVELOPMENT',
+        emailAdd:'earvinjaybermudo21@gmail.com'
     },
     methods: {
         onClickAboutMe: function (event) {
